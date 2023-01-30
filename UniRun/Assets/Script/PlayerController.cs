03c09722a97d34113a21f68c1d20b07464ac7787
BBulSo <108716571+BBulSo@users.noreply.github.com>
2023-01-30 09:41:34 +0900

    [KJH]

---
+++ UniRun/Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 스타트
+    void Start()
+    {
+        
+    }
+
+    // 업데이트
+    void Update()
+    {
+        
+    }
+}
